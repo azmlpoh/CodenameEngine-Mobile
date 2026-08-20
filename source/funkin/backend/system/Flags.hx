fed868e3f69b635ce9cd7251c61e8e04d1e617ae
Sun-dried oranges <1113812931@qq.com>
2026-08-20 18:35:26 +0800

    Remove USE_LEGACY_ZOOM_FACTOR from Flags.hx
    Removed USE_LEGACY_ZOOM_FACTOR variable and its initialization.

--- source/funkin/backend/system/Flags.hx
+++ source/funkin/backend/system/Flags.hx
@@ -164,7 +164,6 @@ class Flags {
 	public static var DEFAULT_CAM_ZOOM_LERP:Float = 0.05;
 	public static var DEFAULT_HUD_ZOOM_LERP:Float = 0.05;
 
-	public static var USE_LEGACY_ZOOM_FACTOR:Null<Bool> = null;
 	
 	// Font configuration
 	public static var DEFAULT_FONT:String = "vcr.ttf";
@@ -318,7 +317,6 @@ class Flags {
 		if (MOD_API_VERSION == null) MOD_API_VERSION = CURRENT_API_VERSION;
 		if (WINDOW_TITLE_USE_MOD_NAME == null) WINDOW_TITLE_USE_MOD_NAME = !overridenFlags.exists('TITLE') && overridenFlags.exists('MOD_NAME');
 		if (USE_LEGACY_TIMING == null) USE_LEGACY_TIMING = MOD_API_VERSION < 2;
-		if (USE_LEGACY_ZOOM_FACTOR == null) USE_LEGACY_ZOOM_FACTOR = MOD_API_VERSION < 2;
 		if (SUSTAINS_AS_ONE_NOTE == null) SUSTAINS_AS_ONE_NOTE = MOD_API_VERSION >= 2;
 		if (USE_LEGACY_CENTER_CAM == null) USE_LEGACY_CENTER_CAM = MOD_API_VERSION < 3;
 		if (USE_LEGACY_FLXANIMATE_STAGE_MATRIX == null) USE_LEGACY_FLXANIMATE_STAGE_MATRIX = MOD_API_VERSION < 3;
