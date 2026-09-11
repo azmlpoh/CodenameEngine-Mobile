5ae6bf3c935f1c843e1521748d12fb3085149374
Sun-dried oranges <1113812931@qq.com>
2026-09-11 21:56:41 +0800

    Refactor asset paths for mods and addons

--- source/funkin/backend/assets/AssetsLibraryList.hx
+++ source/funkin/backend/assets/AssetsLibraryList.hx
@@ -16,7 +16,7 @@ class AssetsLibraryList extends AssetLibrary {
 	}
 
 	public var rootDirectory:String = "./assets";
-	
+
 	// is true if any library in `libraries` contains some kind of compressed library. 
 	public var hasCompressedLibrary(get, never):Bool;
 	function get_hasCompressedLibrary():Bool {
@@ -53,7 +53,6 @@ class AssetsLibraryList extends AssetLibrary {
 		}
 		return lib;
 	}
-
 	var assetPathCacheLibrary:Map<AssetSource, Map<Null<String>, Map<String, AssetLibrary>>> = [];
 	var assetPathCacheTime:Map<AssetSource, Map<Null<String>, Map<String, Float>>> = [];
 
@@ -105,9 +104,9 @@ class AssetsLibraryList extends AssetLibrary {
 				else
 					return library;
 			}
+		}
 
 			cacheTimePaths.set(id, time);
-		}
 
 		for (library in libraries) {
 			if (shouldSkipLib(library, source)) continue;
@@ -237,8 +236,8 @@ class AssetsLibraryList extends AssetLibrary {
 	#if sys
 	inline function switchToSourceAssets() {
 		#if MOD_SUPPORT
-		ModsFolder.modsPath = './${Main.pathBack}mods/';
-		ModsFolder.addonsPath = './${Main.pathBack}addons/';
+		ModsFolder.modsPath = '${Main.pathBack}mods/';
+		ModsFolder.addonsPath = '${Main.pathBack}addons/';
 		#end
 
 		rootDirectory = './${Main.pathBack}assets/';
